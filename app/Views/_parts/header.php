e252217906444119d9667fe1ad7ccb8611334881
Ikhwan <ikhwantris99@gmail.com>
2020-11-16 21:51:01 +0700

    bg header

--- app/Views/_parts/header.php
+++ app/Views/_parts/header.php
@@ -9,6 +9,9 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/bootstrap.css">
 
+    <!-- Font Awesome -->
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    
     <!-- Custom CSS -->
     <link rel="stylesheet" href="assets/css/style_header.css">
     <link rel="stylesheet" href="assets/css/style_beranda.css">
--- app/Views/v_beranda.php
+++ app/Views/v_beranda.php
@@ -1,5 +1,24 @@
+<!-- background header -->
 <div class="hero-image" style="max-height: 90vh;">
     <div class="hero-text">
         <h1 class="font-weight-bold">Pangan adalah pilar hidup <br> matinya sebuah bangsa</h1>
     </div>
 </div>
+
+<!-- Tentang Kami -->
+<section class="mt-4 px-sm-3 container">
+    <div class="row">
+        <div class="col-sm">
+            <h2 class="font-weight-bold">Tentang Kami</h2>
+            <!-- kutipan kalimat tentang kami -->
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia fuga animi ad, quisquam corrupti repellat sunt vero libero? Ullam aliquam eum sequi labore eaque voluptas. Illum dolore sequi placeat eaque!</p>
+            <button class="btn btn-dark">
+                Selengkapnya
+                <i class='fas fa-chevron-right'></i>
+            </button>
+        </div>
+        <div class="col-sm">
+
+        </div>
+    </div>
+</section>
