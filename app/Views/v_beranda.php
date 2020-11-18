cd7c9d95dab89137d28453bdd54ed8db35b8a83e
Ikhwan <ikhwantris99@gmail.com>
2020-11-18 22:41:57 +0700

    tentang kami, tujuan (proses)

--- app/Views/v_beranda.php
+++ app/Views/v_beranda.php
@@ -6,19 +6,38 @@
 </div>
 
 <!-- Tentang Kami -->
-<section class="mt-4 px-sm-3 container">
+<section class="mt-5 px-sm-3 container">
     <div class="row">
         <div class="col-sm">
             <h2 class="font-weight-bold">Tentang Kami</h2>
             <!-- kutipan kalimat tentang kami -->
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia fuga animi ad, quisquam corrupti repellat sunt vero libero? Ullam aliquam eum sequi labore eaque voluptas. Illum dolore sequi placeat eaque!</p>
-            <button class="btn btn-dark">
+            <p class="text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia fuga animi ad,
+                quisquam corrupti repellat sunt vero libero? Ullam aliquam eum sequi labore eaque
+                voluptas. Illum dolore sequi placeat eaque!. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia fuga animi ad,
+            </p>
+            <button class="btn btn-dark btn-hidden">
                 Selengkapnya
                 <i class='fas fa-chevron-right'></i>
             </button>
         </div>
-        <div class="col-sm">
+        <div class="col-sm text-center">
+            <img class="tentang-img img-fluid img-shadow rounded-border" src="assets/images/img_jagung.jpg" alt="">
+            <button class="mt-3 btn btn-dark btn-hidden-2">
+                Selengkapnya
+                <i class='fas fa-chevron-right'></i>
+            </button>
+        </div>
+    </div>
+</section>
 
+<!-- Tujuan -->
+<section class="mt-5 container-fluid bg-green">
+    <div class="row">
+        <div class="px-0 col-sm">
+            <img class=" img-fluid h-75 w-100" src="assets/images/img_beras.jpg" alt="">
+        </div>
+        <div class="col-sm">
+            <h2 class="font-weight-bold text-white">Tujuan</h2>
         </div>
     </div>
 </section>
