a7081c9a8a1a5ba11981ba4b8652d608aeef71fa
fachry53 <46189875+fachry53@users.noreply.github.com>
2020-12-11 15:56:19 +0700

    vardump

--- app/Controllers/Kuesioner.php
+++ app/Controllers/Kuesioner.php
@@ -77,7 +77,7 @@ class Kuesioner extends BaseController
         
         $dataresponse = $q1.','.$q2.','.$q3.','.$q4.','.$q5;
 
-        var_dump($iplong);die;
+        // var_dump($iplong);die;
 
         $data = [
             'responseid' => "",
